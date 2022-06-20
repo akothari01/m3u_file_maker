191f7d378b988c491e78a7290f1ff9f7264f5edb
akothariUNB3035 <74020378+akothari01@users.noreply.github.com>
2022-06-20 18:49:06 +0530

    starting project, gui using easygui, single game only

--- m3u_easygui.py
+++ m3u_easygui.py
@@ -1,20 +1,35 @@
 import easygui
 import os
+import re
 
 
 def getPath() -> str:
     return easygui.diropenbox()
 
 
-def scanDirForFiles(pathDir: str) -> list[str]:
+def scanDirForCueFiles(pathDir: str) -> list[str]:
     obj = os.scandir(pathDir)
     fileNames = list[str]()
     for entry in obj:
-        if entry.is_file():
+        if entry.is_file() and entry.name[-3:] == 'cue':
             fileNames.append(entry.name)
+            fileNames.append('\n')
+    fileNames.pop()
     return fileNames
 
 
+def makeM3UFile(names: list[str], addPath: str):
+    entry1 = names[0]
+    toRemove = re.findall(r'\s\(Disc \d\)', entry1)
+    toStr = ""
+    toStr = toStr.join(toRemove)
+    titleFile = entry1.replace(toStr, '')
+    titleFile = titleFile.replace('.cue', '')
+    m3uFileName = addPath + '\\' + titleFile + '.m3u'
+    m3uFile = open(m3uFileName, "w")
+    m3uFile.writelines(names)
+
+
 path = getPath()
-file_names = scanDirForFiles(path)
-print(file_names)
+file_names = scanDirForCueFiles(path)
+makeM3UFile(file_names, path)
