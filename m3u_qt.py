2ee3a19e1339e6220021dea33fdfd7d1067ac102
akothariUNB3035 <74020378+akothari01@users.noreply.github.com>
2022-06-21 19:13:44 +0530

    almost done, using pyside6 implementation

--- m3u_qt.py
+++ m3u_qt.py
@@ -1,11 +1,11 @@
-import easygui
 import os
 import re
-from PySide6 import QtCore, QtWidgets, QtGui
+import sys
 
-
-def getPath() -> str:
-    return easygui.diropenbox()
+import easygui
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QApplication, QPushButton, QLineEdit, QHBoxLayout, QLabel, \
+    QVBoxLayout, QDialog
 
 
 def scanDirForCueFiles(pathDir: str) -> list[str]:
@@ -19,18 +19,53 @@ def scanDirForCueFiles(pathDir: str) -> list[str]:
     return fileNames
 
 
-def makeM3UFile(names: list[str], addPath: str):
-    entry1 = names[0]
-    toRemove = re.findall(r'\s\(Disc \d\)', entry1)
-    toStr = ""
-    toStr = toStr.join(toRemove)
-    titleFile = entry1.replace(toStr, '')
-    titleFile = titleFile.replace('.cue', '')
-    m3uFileName = addPath + '\\' + titleFile + '.m3u'
-    m3uFile = open(m3uFileName, "w")
-    m3uFile.writelines(names)
+class MainWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.editText = QLineEdit("Select your Directory")
+        self.browseButton = QPushButton("Browse")
+        self.browseButton.clicked.connect(self.getPath)
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(self.editText)
+        hLayout.addWidget(self.browseButton)
+
+        self.askText = QLabel("Is this the correct directory?")
+        self.yesButton = QPushButton("Yes")
+
+        self.yesButton.clicked.connect(self.makeM3UFile)
+        vLayout = QVBoxLayout()
+        vLayout.addLayout(hLayout)
+        vLayout.addWidget(self.askText)
+        vLayout.addWidget(self.yesButton)
+
+        self.setLayout(vLayout)
 
 
-path = getPath()
-file_names = scanDirForCueFiles(path)
-makeM3UFile(file_names, path)
+    @Slot()
+    def getPath(self) -> str:
+        return easygui.diropenbox()
+
+
+    @Slot()
+    def makeM3UFile(self, names: list[str], addPath: str):
+        entry1 = names[0]
+        toRemove = re.findall(r'\s\(Disc \d\)', entry1)
+        toStr = ""
+        toStr = toStr.join(toRemove)
+        titleFile = entry1.replace(toStr, '')
+        titleFile = titleFile.replace('.cue', '')
+        m3uFileName = addPath + '\\' + titleFile + '.m3u'
+        m3uFile = open(m3uFileName, "w")
+        m3uFile.writelines(names)
+
+
+def main():
+    app = QApplication(sys.argv)
+    mWin = MainWindow()
+    mWin.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
